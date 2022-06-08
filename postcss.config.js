faa36f7aa46693893ffd6df8c1f55f57ab8d9d5b
wangchao <18888351756@163.com>
2022-06-08 23:07:16 +0800

    chore: update postcss-px-to-viewport close #3

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,8 @@
 // postcss.config.js
 module.exports = {
   plugins: {
-    'postcss-px-to-viewport': {
+    "autoprefixer": {},
+    'postcss-px-to-viewport-8-plugin': {
       viewportWidth: 375
     }
   }
