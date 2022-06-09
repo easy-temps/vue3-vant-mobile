0a6263c240ea7c945e3db00b8374ed886a0770fe
wangchao <18888351756@163.com>
2022-06-09 13:18:46 +0800

    feat: add git hooks & eslint

---
+++ .eslintrc.js
@@ -0,0 +1,39 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+  },
+  extends: [
+    'plugin:vue/vue3-essential',
+    'eslint:recommended',
+    '@vue/typescript/recommended',
+    '@vue/eslint-config-typescript',
+    'plugin:import/recommended',
+    'plugin:import/typescript',
+  ],
+  plugins: ['import'],
+  parserOptions: {
+    ecmaVersion: 2020,
+  },
+  rules: {
+    'vue/multi-word-component-names': 'off',
+    'import/no-named-as-default': 'off',
+    'import/namespace': [2, { allowComputed: true }],
+    'import/no-named-as-default-member': 'off',
+    'import/no-unresolved': [2, { ignore: ['^@'] }],
+    '@typescript-eslint/consistent-type-imports': 'error',
+    // enUS: all rules docs https://eslint.org/docs/rules/
+    // zhCN: 所有规则文档 https://eslint.bootcss.com/docs/rules/
+    // 基础规则 全部 ES 项目通用
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'quote-props': 'off',
+    // 禁止混合使用不同的操作符 'error','off'
+    'no-mixed-operators': 'off',
+    // 禁止未使用过的变量 default: ['error', { vars: 'local' }]
+    'no-unused-vars': ['off'],
+    // 强制在代码块中开括号前和闭括号后有空格
+    'block-spacing': ['error', 'always'],
+    'object-curly-spacing': ['error', 'always']
+  },
+}
