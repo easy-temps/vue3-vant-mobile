77e1b99f7d878b109b90f8fd62431d42ffd43ba9
wangchao <18888351756@163.com>
2022-06-13 14:00:34 +0800

    wip: support for dark mode

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,30 @@
 <template>
-  <router-view />
+  <van-config-provider :theme="theme">
+    <van-switch v-model="checked" />
+    <router-view />
+  </van-config-provider>
 </template>
+
+<script lang="ts">
+import { defineComponent, ref, watch } from 'vue'
+export default defineComponent({
+  setup() {
+    const theme = ref('dark')
+    const checked = ref(true)
+
+    watch(
+      checked,() => {
+        if(checked.value) {
+          document.getElementsByTagName('html')[0].setAttribute('data-theme', 'data-theme-dark')
+        } else {
+          document.getElementsByTagName('html')[0].setAttribute('data-theme', 'data-theme-light')
+        }
+    }, { immediate: true })
+    
+    return {
+      theme,
+      checked
+    }
+  }
+})
+</script>
--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import App from './App.vue'
 import router from './router'
 import { createPinia } from 'pinia'
 
+import './app.less'
 import Vant from 'vant'
 import 'vant/lib/index.css'
 
