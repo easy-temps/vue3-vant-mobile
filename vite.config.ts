03bdf5c44f94f9e6801a1e21821bb165ab99ced4
wangchao <18888351756@163.com>
2022-06-23 15:08:08 +0800

    ci(plugins): support for legacy browsers :green_heart:

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,7 @@
+import path from 'path'
 import { loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import legacy from '@vitejs/plugin-legacy'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import type { ConfigEnv, UserConfig } from 'vite'
 import { visualizer } from 'rollup-plugin-visualizer'
@@ -7,27 +9,34 @@ import Components from 'unplugin-vue-components/vite'
 import { viteVConsole } from 'vite-plugin-vconsole'
 import { VantResolver } from 'unplugin-vue-components/resolvers'
 import createMockServer from './build/mockServer'
-import path from 'path'
 
-// https://vitejs.dev/config/
 export default ({ command, mode }: ConfigEnv): UserConfig => {
   const root = process.cwd()
   const env = loadEnv(mode, root)
+
   return {
     base: env.VITE_APP_PUBLIC_PATH,
+
     define: {
       'process.env.VUE_APP_API_BASE_URL': JSON.stringify(env.VITE_APP_API_BASE_URL),
-      'process.env.VUE_APP_PUBLIC_PATH': JSON.stringify(env.VITE_APP_PUBLIC_PATH),
+      'process.env.VUE_APP_PUBLIC_PATH': JSON.stringify(env.VITE_APP_PUBLIC_PATH)
     },
+
     plugins: [
       vue(),
       vueJsx(),
       visualizer(),
+
+      legacy({
+        targets: ['defaults', 'not IE 11']
+      }),
+
       Components({
         dts: true,
         resolvers: [VantResolver()],
-        types: [],
+        types: []
       }),
+
       viteVConsole({
         entry: [path.resolve('src/main.ts')],
         localEnabled: command === 'serve',
@@ -38,24 +47,27 @@ export default ({ command, mode }: ConfigEnv): UserConfig => {
         }
       })
     ],
+    
     resolve: {
       alias: {
         '~@': path.join(__dirname, './src'),
         '@': path.join(__dirname, './src'),
-        '~': path.join(__dirname, './src/assets'),
-        vue: 'vue/dist/vue.esm-bundler.js',
-      },
+        '~': path.join(__dirname, './src/assets')
+      }
     },
+
     server: {
       host: true,
+      port: 3000,
+      open: true,
       proxy: {
         '/api': {
           // backend url
           target: env.VITE_HTTP_MOCK && env.VITE_MOCK ? createMockServer() : '',
           ws: false,
-          changeOrigin: true,
-        },
-      },
+          changeOrigin: true
+        }
+      }
     }
   }
 }
