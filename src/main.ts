ce793042fcdb34d24630008428fb171220052a74
CharleeWa <18888351756@163.com>
2022-11-14 16:55:06 +0800

    chore: :ambulance: import style of function components

--- src/main.ts
+++ src/main.ts
@@ -9,6 +9,17 @@ import './app.less'
 // Vant 桌面端适配
 import '@vant/touch-emulator'
 
+/* --------------------------------
+Vant 中有个别组件是以函数的形式提供的，
+包括 Toast，Dialog，Notify 和 ImagePreview 组件。
+在使用函数组件时，unplugin-vue-components
+无法自动引入对应的样式，因此需要手动引入样式。
+------------------------------------- */
+import 'vant/es/toast/style'
+import 'vant/es/dialog/style'
+import 'vant/es/notify/style'
+import 'vant/es/image-preview/style'
+
 const app = createApp(App)
 const pinia = createPinia()
 pinia.use(piniaPluginPersistedstate)
