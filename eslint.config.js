cb7371e1ada1021d5e8c253617bb9147c74c13aa
CharleeWa <18888351756@163.com>
2023-12-25 21:48:38 +0800

    chore: Add netlify.toml configuration

--- eslint.config.js
+++ eslint.config.js
@@ -4,4 +4,10 @@ export default antfu({
   // enable UnoCSS support
   // https://unocss.dev/integrations/vscode
   unocss: true,
+
+  ignores: [
+    'netlify.toml',
+    'node_modules',
+    'dist',
+  ],
 })
