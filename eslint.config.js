608337148a69851c6809eb83f791dd26ffe687da
CharleeWa <18888351756@163.com>
2023-12-26 17:31:18 +0800

    chore: Update eslint ignore rules

--- eslint.config.js
+++ eslint.config.js
@@ -4,10 +4,4 @@ export default antfu({
   // enable UnoCSS support
   // https://unocss.dev/integrations/vscode
   unocss: true,
-
-  ignores: [
-    'netlify.toml',
-    'node_modules',
-    'dist',
-  ],
 })
