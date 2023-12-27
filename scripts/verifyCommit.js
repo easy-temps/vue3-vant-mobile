c15c88405364fdf77c7629d257e23281add2152a
CharleeWa <18888351756@163.com>
2023-12-27 14:26:22 +0800

    refactor: Explicitly import `node:process`

--- scripts/verifyCommit.js
+++ scripts/verifyCommit.js
@@ -1,4 +1,5 @@
 import fs from 'node:fs'
+import process from 'node:process'
 
 const msg = fs.readFileSync('.git/COMMIT_EDITMSG', 'utf-8').trim()
 
@@ -9,7 +10,6 @@ if (!commitRE.test(msg)) {
   if (!mergeRe.test(msg)) {
     console.log('git commit unpass')
     console.error('git commit error, needs title(scope): desc')
-    // eslint-disable-next-line node/prefer-global/process
     process.exit(1)
   }
 }
