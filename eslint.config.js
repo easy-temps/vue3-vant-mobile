1c11846cacd799671e65d99a8f4b8e1f7a2af8fc
CharleeWa <18888351756@163.com>
2024-03-02 13:25:38 +0800

    chore: use js config file for eslint

--- eslint.config.js
+++ eslint.config.js
@@ -1,5 +1,7 @@
 import antfu from '@antfu/eslint-config'
 
+// We temporarily disable `eslint.config.ts` which given by `eslint-ts-patch`
+// Related: https://github.com/antfu/eslint-ts-patch/issues/5
 export default antfu({
   // enable UnoCSS support
   // https://unocss.dev/integrations/vscode
