59162e96165b9a64903e3c86b8e323815dcbb264
CharleeWa <18888351756@163.com>
2024-07-20 13:27:49 +0800

    chore: Update all deps

--- src/env.d.ts
+++ src/env.d.ts
@@ -3,7 +3,7 @@
 declare module '*.vue' {
   import type { DefineComponent } from 'vue'
 
-  // eslint-disable-next-line ts/ban-types
+  // eslint-disable-next-line ts/no-empty-object-type
   const component: DefineComponent<{}, {}, any>
   export default component
 }
