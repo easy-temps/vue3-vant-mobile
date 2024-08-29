468125e65fe50981355eee56cbfe81d2f08688cb
Charlie ✨ <18888351756@163.com>
2024-08-30 00:07:20 +0800

    chore: Add code comment

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router/auto'
-import { handleHotUpdate, routes } from 'vue-router/auto-routes'
+import { routes, handleHotUpdate } from 'vue-router/auto-routes'
 
 import NProgress from 'nprogress'
 import 'nprogress/nprogress.css'
@@ -14,9 +14,12 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.VITE_APP_PUBLIC_PATH),
   routes,
 })
-if(import.meta.hot) {
-  handleHotUpdate(router)
+
+// This will update routes at runtime without reloading the page
+if (import.meta.hot) { 
+  handleHotUpdate(router) 
 }
+
 router.beforeEach((to: EnhancedRouteLocation, from, next) => {
   NProgress.start()
 
