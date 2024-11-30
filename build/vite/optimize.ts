4befbabe64f92d211f9d2a6819fac1e854b3d8f8
CharleeWa <18888351756@163.com>
2024-11-30 20:34:29 +0800

    chore: Update vite deps optimization config

--- build/vite/optimize.ts
+++ build/vite/optimize.ts
@@ -3,6 +3,7 @@ const include = [
   'echarts',
   'lodash-es',
   'resize-detector',
+  'vant/es',
   'vant/es/cell-group/style/index',
   'vant/es/popup/style/index',
   'vant/es/picker/style/index',
@@ -17,6 +18,13 @@ const include = [
   'vant/es/form/style/index',
   'vant/es/field/style/index',
   'vant/es/notify/style/index',
+  'vant/es/config-provider/style/index',
+  'vant/es/nav-bar/style/index',
+  'vant/es/tabbar/style/index',
+  'vant/es/tabbar-item/style/index',
+  'unplugin-vue-router/runtime',
+  'unplugin-vue-router/data-loaders',
+  'unplugin-vue-router/data-loaders/basic',
 ]
 
 const exclude = [
