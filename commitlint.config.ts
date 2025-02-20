831732c719884a9e69432f539557d9fbb9099a5d
CharleeWa <18888351756@163.com>
2025-02-20 22:12:54 +0800

    chore: replace husky with simple-git-hooks (#143)

---
+++ commitlint.config.ts
@@ -0,0 +1,32 @@
+import type { UserConfig } from '@commitlint/types'
+import { RuleConfigSeverity } from '@commitlint/types'
+
+const Configuration: UserConfig = {
+  extends: ['@commitlint/config-conventional'],
+  formatter: '@commitlint/format',
+  rules: {
+    'type-enum': [
+      RuleConfigSeverity.Error,
+      'always',
+      [
+        'feat',
+        'fix',
+        'perf',
+        'style',
+        'docs',
+        'test',
+        'refactor',
+        'build',
+        'ci',
+        'chore',
+        'revert',
+        'wip',
+        'workflow',
+        'types',
+        'release',
+      ],
+    ],
+  },
+}
+
+export default Configuration
