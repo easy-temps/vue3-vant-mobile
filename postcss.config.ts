3cbd8d5d3fc7b45b3dafc321c4d3f65e0a0b96b9
CharleeWa <18888351756@163.com>
2025-04-01 16:01:41 +0800

    chore: update deps

--- postcss.config.ts
+++ postcss.config.ts
@@ -1,4 +1,16 @@
 // 此文件不支持热更新，修改后需要重启生效
+
+// 需要转换的 fixed 选择器列表
+const rootContainingBlockSelectorList = [
+  '.van-tabbar',
+  '.van-popup',
+  '.van-popup--bottom',
+  '.van-popup--top',
+  '.van-popup--left',
+  '.van-popup--right',
+  // 在这里添加你的选择器
+]
+
 export default {
   plugins: {
     'autoprefixer': {},
@@ -9,10 +21,7 @@ export default {
       viewportWidth: 375,
       maxDisplayWidth: 600,
       border: true,
-      rootContainingBlockSelectorList: [
-        'van-tabbar',
-        'van-popup',
-      ],
+      rootContainingBlockSelectorList,
     },
   },
 }
